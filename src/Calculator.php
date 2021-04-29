ee0e4c329fd893defbb46b129a391383d642de6d
venturacodes <venruracodes@gmail.com>
2021-04-29 08:17:28 -0300

    Created Test and Feature folder, just like in documentation. Created Calculator Class, responsible for calculate stuff and CalculatorTest.php in Unit Folder, since I want to unittest. Added PSR-4 to composer.json and created my first test in CalculatorTest.php. Added method sum($value1, $value2) to Calculator.php

--- src/Calculator.php
+++ src/Calculator.php
@@ -3,4 +3,8 @@ namespace App;
 
 class Calculator
 {
+    public function sum($value1,$value2)
+    {
+        return $value1 + $value2;
+    }
 }
--- tests/Unit/CalculatorTest.php
+++ tests/Unit/CalculatorTest.php
@@ -1,1 +1,6 @@
 <?php
+use App\Calculator;
+test('assert sum 1+1 equals 2', function(){
+    $calculator = new Calculator();
+    $this->assertEquals(2, $calculator->sum(1,1));
+});
