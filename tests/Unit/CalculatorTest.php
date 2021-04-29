52d6232ce30284cdb1cb792de7f88d44edbb3dcf
venturacodes <venruracodes@gmail.com>
2021-04-29 10:32:43 -0300

    Added a setup method for the CalculatorTest.php

--- tests/Unit/CalculatorTest.php
+++ tests/Unit/CalculatorTest.php
@@ -1,27 +1,24 @@
 <?php
 use App\Calculator;
+
+beforeEach(fn()=>$this->calculator = new Calculator());
+
 test('assert sum 1+1 equals 2', function(){
-    $calculator = new Calculator();
-    $this->assertEquals(2, $calculator->sum(1,1));
+    $this->assertEquals(2, $this->calculator->sum(1,1));
 });
 
 test("assert sum 1+1+1 equals 3", function(){
-    $calculator = new Calculator();
-    $this->assertEquals(3, $calculator->sum(1,1,1));
+    $this->assertEquals(3, $this->calculator->sum(1,1,1));
 });
 test("assert sum 1 argument returns the argument", function(){
-    $calculator = new Calculator();
-    $this->assertEquals(10, $calculator->sum(10));
+    $this->assertEquals(10, $this->calculator->sum(10));
 });
 test("assert sum with no argument returns ZERO", function(){
-    $calculator = new Calculator();
-    $this->assertEquals(0, $calculator->sum());
+    $this->assertEquals(0, $this->calculator->sum());
 });
 test("if passed word still works with other params", function(){
-    $calculator = new Calculator();
-    $this->assertEquals(10, $calculator->sum(5,5,"cinco"));
+    $this->assertEquals(10, $this->calculator->sum(5,5,"cinco"));
 });
 test("if passed number as string still works.", function(){
-    $calculator = new Calculator();
-    $this->assertEquals(15, $calculator->sum(5,5,"5"));
+    $this->assertEquals(15, $this->calculator->sum(5,5,"5"));
 });
